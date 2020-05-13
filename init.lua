570b61bcfcc6ab91c8f4a25b1aaae265e58d2aba
Victor Diego Alegandro Diaz Urbaneja <sodomon2@gmail.com>
2020-05-12 20:38:12 -0400

    cambiando de gtk2 a gtk3

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/lua
 --[[!
- @package   
+ @package   run
  @filename  init.lua
  @version   1.0
  @autor     Diaz Urbaneja Victor Diego Alejandro <sodomon2@gmail.com>
@@ -9,21 +9,21 @@
 
 -- declaro mis variables globales
 
-lgi = require('lgi')            -- requiero esta libreria para que me permitira usar GTK
-GObject = lgi.GObject           -- parte de lgi
-GLib = lgi.GLib                 -- para el treeview
-Gtk = lgi.require('Gtk', '2.0') -- el objeto GTK
+lgi     = require('lgi')            	-- requiero esta libreria para que me permitira usar GTK
+GObject = lgi.GObject           	-- parte de lgi
+GLib    = lgi.GLib                 	-- para el treeview
+Gtk     = lgi.require('Gtk', '3.0') 	-- el objeto GTK
 
-assert = lgi.assert
+assert  = lgi.assert
 builder = Gtk.Builder()
 
 assert(builder:add_from_file('run.ui'),"error al cargar el archivo") -- hago un debugger, si este archivo existe (true) enlaso el archivo ejemplo.ui, si no existe (false) imprimo un error
 ui = builder.objects
 
-local main_window = ui.main_window
+local main_window  = ui.main_window
 
-local buscador = ui.input_exec
-local button_exec = ui.btn_exec
+local buscador     = ui.input_exec
+local button_exec  = ui.btn_exec
 local button_close = ui.btn_close
 
 -- al presionar el boton button_exec
